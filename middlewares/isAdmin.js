fdf1a41edd6895c9d89fe88ee26834839a6cc127
gautam <buddhgautam777@gmail.com>
2023-04-02 11:43:19 +0530

    First commit

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -5,7 +5,9 @@ const isAdmin = async(req, res, next) => {
     //find the login user
     const user = await User.findById(req.userAuthId)
     //check if admin
-    if(user.isAdmin){
+    // console.log(user);
+    // console.log(user?.isAdmin);
+    if(user?.isAdmin){
         next();
     }else{
         next(new Error("access denined, admin only"))
